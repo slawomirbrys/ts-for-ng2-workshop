c16790511448278e5fbabeabbb029e045cb92efb
Piotr Woloszun <piotr.woloszun@gmail.com>
2016-02-18 10:58:08 +0100

    v.0.1.1

--- src/02_classes/04_admin-task.ts
+++ src/02_classes/04_admin-task.ts
@@ -15,7 +15,7 @@
  * - encodePassword(passwd) (encoded using private static property ENCODING_STR)
  * Private static properties:
  * - NEXT_UUID
- * - ENCODING_KEY
+ * - ENCODING_STR
  * Private static methods:
  * - generateUuid() (generates next uniq UUID)
  * */
@@ -25,19 +25,19 @@ function adminTest() {
 
   // TODO: uncomment
   /*
-   var admin:Admin = new Admin({
-   login: login,
-   password: passwd
-   });
+  var admin:Admin = new Admin({
+    login: login,
+    password: passwd
+  });
 
-   console.assert(admin.uuid == 0);
-   console.assert(admin.getLogin() == login);
-   login = "kate";
-   admin.setLogin(login);
-   console.assert(admin.getLogin() == login);
-   console.assert(admin.getEncodedPassword() == passwd + "imba!");
-   console.log("adminTest passed");
-   */
+  console.assert(admin.uuid == 0, "uuid should eq 0");
+  console.assert(admin.getLogin() == login, "getLogin() should eq login");
+  login = "kate";
+  admin.setLogin(login);
+  console.assert(admin.getLogin() == login, "setLogin() should change login");
+  console.assert(admin.getEncodedPassword() == passwd + "imba!", "getEncodedPassword() should encode password");
+  console.log("adminTest passed");
+  */
 }
 
 adminTest();
