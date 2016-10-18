4900aa83dfc4be5215d6a6b33482c0650650e860
Sławomir Bryś <slawomir.brys@mbank.pl>
2016-10-18 12:16:49 +0200

    Generics - done.

--- src/04_generics/01_generics_functions.ts
+++ src/04_generics/01_generics_functions.ts
@@ -3,8 +3,26 @@
 // TODO 2: FilterIterator interface; gFilter function
 
 // TODO 3: AllIterator interface; gAll function
+interface IAllIterator<T, R> {
+    (item: T, index: number): R;
+}
+
+function Iterate<T, K>(list: T[], mapper: IAllIterator<T, K>): K[] {
+    let iteratedList: K[];
+
+    list.forEach(function (el: T, i: number) {
+        let iteratedEl: K = mapper(el, i);
+        iteratedList.push(iteratedEl);
+    });
+
+    return iteratedList;
+}
+let fnMapper: IAllIterator<string, boolean> = function (names: string, i: number): boolean {
+    return true;
+};
 
 // TODO 4: SliceIterator interface; gEachSlice function
+
 
 // TODO 5: ReduceIterator interface; gReduce function
 
